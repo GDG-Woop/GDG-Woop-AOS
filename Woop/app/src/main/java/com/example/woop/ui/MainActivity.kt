b27077206ab22ca5576e0c1b6cd35af3700583ba
최우성 <vov1252@naver.com>
2022-06-25 22:48:20 +0900

    - 홈 화면

--- Woop/app/src/main/java/com/example/woop/ui/MainActivity.kt
+++ Woop/app/src/main/java/com/example/woop/ui/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.woop.ui
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.studymatchingapp.ui.base.BaseActivity
 import com.example.woop.R
+import com.example.woop.databinding.ActivityMainBinding
+import com.example.woop.ui.item.WallItem
+import com.xwray.groupie.GroupieAdapter
+import com.xwray.groupie.Section
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setOnView()
+    }
+
+    private fun setOnView() {
+        var groupAdapter = GroupieAdapter()
+        val buildingSection = Section()
+
+        val floor = 15
+        repeat(floor) {
+            repeat(4) {
+                buildingSection.add(WallItem())
+            }
+        }
+
+        groupAdapter.add(buildingSection)
+
+        binding.rvBuilding.adapter = groupAdapter
+        val staggeredGridLayoutManager = StaggeredGridLayoutManager(4, LinearLayoutManager.VERTICAL)
+        binding.rvBuilding.layoutManager = staggeredGridLayoutManager
+
+        groupAdapter.spanSizeLookup
     }
 }
